49180872e8f48f5fb753e05e705fdfbe0842f2a2
SREENATH CHANDRA <srinathchandrae25@gmail.com>
2016-12-11 03:09:55 -0600

    just sove the redirect

--- node.js
+++ node.js
@@ -8,9 +8,9 @@ app.set('view engine','ejs');
 app.use('/asserts',express.static('asserts'));
 
 
-app.get('/',function(req,res){                      // Get HTTP methos
+ app.get('/',function(req,res){                      // Get HTTP methos
   res.render('index');
-});
+ });
 
                                   // POST HTTP method
 app.post('/',urlencodedParser,function(req,res,next){
@@ -29,17 +29,19 @@ app.post('/',urlencodedParser,function(req,res,next){
       subject:'Welcome',
       text:text,
       };
-      transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        console.log(error);
-      return  res.redirect('/');
-    }
-    else{
-        console.log('Message sent: ' + info.response);                       // condiotn loop
-      return  res.redirect('/');
-    };
+     transporter.sendMail(mailOptions, function(error, info){
+  //  if(error){
+    //    console.log(error);
+    //  return  res.redirect('/');
+  //  }
+    //else{
+      //  console.log('Message sent: ' + info.response);                       // condiotn loop
+    //  return  res.redirect('/sree');
+    //};
 });
-  return  res.render('success',{data:req.body});                             // single page application
+
+   res.render('success',{data:req.body});
+                           // single page application
 });
 app.listen(process.env.PORT || 1100,function(){
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);         // port
